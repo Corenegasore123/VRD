1fc1fb6b02c92b2f84626960e4ae92f4e7afebeb
Coreyne <corenegasore@gmail.com>
2024-09-21 18:25:07 +0600

    added feature-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import FeatureSection from "./components/FeatureSection"
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 
@@ -8,6 +9,7 @@ const App = () => {
         <Header />
         <div className="max-w-7xl mx-auto pt-20 px-6">
           <HeroSection />
+          <FeatureSection />
         </div>
       </>
     </div>
