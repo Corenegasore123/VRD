5568898ca82ca1248fcc968d118b970f6143956d
Coreyne <corenegasore@gmail.com>
2024-09-21 18:23:20 +0600

    added hero-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
 
 const App = () => {
   return (
     <div>
       <>
         <Header />
+        <div className="max-w-7xl mx-auto pt-20 px-6">
+          <HeroSection />
+        </div>
       </>
     </div>
   )
